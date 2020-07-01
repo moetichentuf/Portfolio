3853f69c94e6e9c7a386aae9d54ef04e7e9437e1
moeti <moetichentuf@hotmail.com>
2020-07-01 12:37:27 +0200

    test

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,19 @@
 import React, {useState} from 'react';
 import {useTransition, animated, useSpring} from 'react-spring'
+// 3d image effect
 const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
-const Home = ({name, paragraph, authorImage})=>{
-    const [props, set1] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
 
+
+const Home = ({name, paragraph, authorImage})=>{
+    // 3d image effect
+    const [props, set1] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+    // back to top button
     const [, setY] = useSpring(() => ({ y: 0 }))
+    // toogle between divs
     const [toggle, set] = useState([false])
     const transition = useTransition(toggle, null, {
-        from: { position: 'absolute', opacity: 0, fontSize:'2vw', bottom:50},
+        from: { position: 'absolute', opacity: 0, fontSize:'6vw'},
         enter: { opacity: 1 },
         leave: { opacity: 0 },
     })
@@ -18,11 +23,11 @@ const Home = ({name, paragraph, authorImage})=>{
         <div className="home_container">
 
             <div className="home_head_wrapper">
-                <a style={{zIndex:"1"}} onMouseEnter={() => set(!toggle)}>    < h1 style={{cursor:'pointer'}} > Hello I 'm <br></br> <span>{name}</span></h1></a>
+                <a style={{zIndex:"1"}} onMouseEnter={() => set(!toggle)}>    < h1 style={{cursor:'pointer'}} > Hello I 'm <br></br> <span> {transition.map(({ item, key, props }) =>
+                    item ? <animated.div  style={props}>Moeti</animated.div> : <animated.div style={props}>Thibaut</animated.div>
+                )}</span></h1></a>
 
-                {transition.map(({ item, key, props }) =>
-                    item ? <animated.div style={props}>Grahphic Design</animated.div> : <animated.div style={props}>Web Development</animated.div>
-                )}
+
                 {/*<p>{paragraph}</p>*/}
             </div>
             <div className="image_container">
